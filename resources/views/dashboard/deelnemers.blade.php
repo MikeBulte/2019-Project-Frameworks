1f02e82aa844a0392f478ef32ad02b7e34def596
jasperagelink <s1149961@student.windesheim.nl>
2019-12-02 16:18:19 +0100

    deelnemers rows en dynamisch inladen + images toegevoegd

--- resources/views/dashboard/deelnemers.blade.php
+++ resources/views/dashboard/deelnemers.blade.php
@@ -5,6 +5,33 @@
         <div class="height-inner">
             <h1>Deelnemers</h1>
             <div class="container">
+                <div class="row leaderboard-inner">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Tafelnummmer</th>
+                                <th scope="col">Speler 1</th>
+                                <th scope="col">Speler 1</th>
+                                <th scope="col">Speler 1</th>
+                                <th scope="col">Speler 1</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Berry Rotunda</td>
+                                <td>Berry Rotunda</td>
+                                <td>Berry Rotunda</td>
+                                <td>Berry Rotunda</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+
+
+
+
                 <table>
                     <tr>
                         <th scope="col">Voornaam</th>
