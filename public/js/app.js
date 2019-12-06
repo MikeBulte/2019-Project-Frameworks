8735ca0bc11ee63a74a2dba4f0630df2a9740a00
jasperagelink <s1149961@student.windesheim.nl>
2019-12-06 14:32:20 +0100

    file change before merge

--- public/js/app.js
+++ public/js/app.js
@@ -36985,8 +36985,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/Menno/Projecten/999games/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/Menno/Projecten/999games/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/jasperagelink/Projecten/2019_ADSD_Project_Frameworks_Team_01/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/jasperagelink/Projecten/2019_ADSD_Project_Frameworks_Team_01/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
