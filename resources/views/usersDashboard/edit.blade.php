aeb7fc506b0c9d4aef4942670001ff3212fee941
jasperagelink <s1149961@student.windesheim.nl>
2019-12-09 16:51:57 +0100

    edit page styling, styling construct

--- resources/views/usersDashboard/edit.blade.php
+++ resources/views/usersDashboard/edit.blade.php
@@ -13,63 +13,69 @@
                         <h6 class="alert alert-danger text-center"> {{ session('alert') }}</h6>
                     @endif
                     <h1>Profielinstellingen</h1>
-                    <form action="{{ route('usersdashboard.update', ['usersdashboard' => $user]) }}"
-                          method="post">
-                        @csrf
-                        @method('PUT')
-                        <div class="row">
-                            <div class="col-6">
-                                <label for="first_name">
-                                    <h2>Wijzig voornaam</h2>
-                                </label>
-                            </div>
-                            <div class="col-6">
-                                <label for="last_name">
-                                    <h2>Wijzig achternaam</h2>
-                                </label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-6">
-                                <input class="col-12" type="text" id="first_name" name="first_name"
-                                       value="{{ $user->first_name }}">
-                            </div>
-                            <div class="col-6">
-                                <input class="col-12" type="text" id="last_name" name="last_name"
-                                       value="{{ $user->last_name }}">
-                            </div>
-                            <input type="hidden" name="email" value="{{ $user->email }}">
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <label for="password">
-                                    <h2>Wijzig wachtwoord</h2>
-                                </label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-6">
-                                <input class="col-12" id="password" type="password" name="password"
-                                       placeholder="Nieuw wachtwoord">
-                            </div>
-                            <div class="col-6">
-                                <input class="col-12" type="password" name="password_confirmation"
-                                       placeholder="Herhaal nieuw wachtwoord">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <button class="scnd-btn"><input type="submit" value="Wijzigingen opslaan"></button>
-                            </div>
-                        </div>
+                    <div class="settings">
+                        <form action="{{ route('usersdashboard.update', ['usersdashboard' => $user]) }}" method="post">
+                            @csrf
+                            @method('PUT')
 
-                    </form>
+                            <div class="change-name">
+                                <div class="row">
+                                    <div class="col-lg">
+                                        <label for="first_name">
+                                            <h2>Wijzig voornaam</h2>
+                                        </label>
+                                    </div>
+                                    <div class="col-lg">
+                                        <label for="last_name">
+                                            <h2>Wijzig achternaam</h2>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg">
+                                        <input type="text" id="first_name" name="first_name"
+                                               value="{{ $user->first_name }}">
+                                    </div>
+                                    <div class="col-lg stubborn-input">
+                                        <input type="text" id="last_name" name="last_name"
+                                               value="{{ $user->last_name }}">
+                                    </div>
+                                    <input type="hidden" name="email" value="{{ $user->email }}">
+                                </div>
+                            </div>
+                            <div class="change-password">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <label for="password">
+                                            <h2>Wijzig wachtwoord</h2>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg">
+                                        <input id="password" type="password" name="password"
+                                               placeholder="Nieuw wachtwoord">
+                                    </div>
+                                    <div class="col-lg">
+                                        <input type="password" name="password_confirmation"
+                                               placeholder="Herhaal nieuw wachtwoord">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <button class="scnd-btn"><input type="submit" value="Wijzigingen opslaan"></button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
                 </div>
             </div>
             <div class="row settings-sign-out">
                 <div class="col-12">
-                    <h1>Uitschrijven</h1>
-                    <h2>Ik wil mij graag uitschrijven van het NK Carcassonne</h2>
+                    <h2 class="h1">Uitschrijven</h2>
+                    <h3>Ik wil mij graag uitschrijven van het NK Carcassonne</h3>
                     <form action="{{ route('usersdashboard.destroy', ['usersdashboard' => $user]) }}"
                           method="post">
                         @csrf
