ad7d1b676e50a1533562f3f3e1cc4c6d2f254cd4
Menno <s1149975@student.windesheim.nl>
2019-12-10 13:53:51 +0100

    add icons

--- resources/views/dashboard/newsfeed.blade.php
+++ resources/views/dashboard/newsfeed.blade.php
@@ -70,7 +70,44 @@
                             <td scope="row">{{ $newsfeed->id }}</td>
                             <td style="width: 10%">{{ $newsfeed->created_at->format('d-m-y')}}</td>
                             <td>{{ $newsfeed->title}}</td>
-                            <td><img style="width: 20%" src="{{ asset('storage/icons/ic_edit.png')}}"></td>
+                            <td><img style="width: 20%" data-toggle="modal" data-target="#UpdateModal" src="{{ asset('storage/icons/ic_edit.png')}}"></td>
+                            <div class="modal" id="UpadateModal" tabindex="-1" role="dialog"
+                                 aria-labelledby="AddPlayersModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="AddNewsArticle">Nieuwsbericht editen</h5>
+                                            <button type="button" class="close" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form method="post" action="{{ route('nieuwsfeed.store') }}">
+
+                                                @csrf
+                                                <input class="form-control" type="text" name="title" placeholder="Titel"
+                                                       required>
+                                                <br>
+                                                <textarea class="form-control" type="text" name="article" placeholder="Content"
+                                                          required rows="15"></textarea>
+                                                <br>
+                                                <button class="" type="submit" name="image" value="Upload image">Upload hier je
+                                                    afbeelding
+                                                </button>
+
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn scnd-btn" data-dismiss="modal">Close
+                                                    </button>
+                                                    <button type="submit" name="Submit" value="Add new article"
+                                                            class="btn prim-btn">Save changes
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
                             <td>
                                 <form action="{{ route('nieuwsfeed.destroy', ['nieuwsfeed' => $newsfeed]) }}"
                                       method="post">
