6fb8521026cf7603606ea1180c64988e25df8a3d
Mike <mike.bulte@windesheim.nl>
2020-01-11 15:12:01 +0100

    Changed round edit styling to be a bit more outlined

--- resources/views/dashboard/rounds/edit.blade.php
+++ resources/views/dashboard/rounds/edit.blade.php
@@ -13,11 +13,18 @@
                 <form method="POST" action="{{ route('rounds.update', ['round' => $round]) }}">
                     @csrf
                     @method('PUT')
-
-                    <input class="w-100 mb-4" type="text" name="name" placeholder="Ronde naam" minlength="5" maxlength="45" value="{{ $round->name }}" required>
-                    <input class="w-100 mb-4" type="checkbox" id="bracket_round" name="bracket_round" value="1" {{ ($round->bracket_round ? 'checked' : '') }}><label for="bracket_round">Bracket ronde</label>
-                    <input class="w-100 mb-4" type="checkbox" id="in_progress" name="in_progress" value="1" {{ ($round->in_progress ? 'checked' : '') }}><label for="in_progress">Huidig bezig</label>
-                    <input class="w-100 mb-4" type="checkbox" id="finished" name="finished" value="1" {{ ($round->finished ? 'checked' : '') }}><label for="finished">Afgelopen</label>
+                    <div class="w-100">
+                        <input class="w-100 mr-3 mb-4" type="text" name="name" placeholder="Ronde naam" minlength="5" maxlength="45" value="{{ $round->name }}" required>
+                    </div>
+                    <div class="w-100">
+                        <input class="mr-3 mb-4" type="checkbox" id="bracket_round" name="bracket_round" value="1" {{ ($round->bracket_round ? 'checked' : '') }}><label class="w-75" for="bracket_round">Bracket ronde</label>
+                    </div>
+                    <div class="w-100">
+                        <input class="mr-3 mb-4" type="checkbox" id="in_progress" name="in_progress" value="1" {{ ($round->in_progress ? 'checked' : '') }}><label class="w-75" for="in_progress">Huidig bezig</label>
+                    </div>
+                    <div class="w-100">
+                        <input class="mr-3 mb-4" type="checkbox" id="finished" name="finished" value="1" {{ ($round->finished ? 'checked' : '') }}><label class="w-75" for="finished">Afgelopen</label>
+                    </div>
 
                     <div class="modal-footer">
                         <button class="btn scnd-btn">
